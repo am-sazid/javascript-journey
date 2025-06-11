f40cef33237dca9246071ce2b051f7de34e37d31
am-sazid <amsazid.cse@gmail.com>
2025-06-11 23:17:47 +0600

    type of variable

---
+++ intro-js/type.js
@@ -0,0 +1,7 @@
+
+var number = 1213;
+console.log(typeof number); // "number"
+var isTrue = true;
+console.log(typeof isTrue); // "boolean"
+var text = "Hello, world!";
+console.log(typeof text); // "string"

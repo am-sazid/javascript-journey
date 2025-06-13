a9112d04cecab1d47c15ab5aabd59e9ac7803e21
am-sazid <amsazid.cse@gmail.com>
2025-06-13 22:34:00 +0600

    Task 1

---
+++ intro-js/task.js
@@ -0,0 +1,29 @@
+// task 1
+var money = 1000;
+var cost = 800;
+
+
+console.log("returns: " + (money - cost));
+
+// task 2
+var Mathematics = 90;
+var Physics = 80;
+var Chemistry = 85;
+var English = 75;
+var ComputerScience = 95;
+var totalMarks = Mathematics + Physics + Chemistry + English + ComputerScience;
+var averageMarks = totalMarks / 5;
+console.log("average marks: " + averageMarks);
+
+// task 3
+var number = 47;
+var modulus = number % 5;
+console.log("modulus: " + modulus);
+
+// task 4
+
+var a = isNaN(11);
+console.log(a)
+
+var a = isNaN(2-10);
+console.log(a)

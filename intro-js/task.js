989342345d792cc77e4aaf8e6f62d701534c283d
am-sazid <amsazid.cse@gmail.com>
2025-06-15 23:49:15 +0600

    compare python and javascript

--- intro-js/task.js
+++ intro-js/task.js
@@ -27,3 +27,4 @@ console.log(a)
 
 var a = isNaN(2-10);
 console.log(a)
+
--- intro-js/type.js
+++ intro-js/type.js
@@ -7,3 +7,6 @@ var text = "Hello, world!";
 console.log(typeof text); // "string"
 var nothing = null;
 console.log(typeof nothing); // "object" (this is a known JavaScript quirk)
+
+var condition = 5 > 3;
+console.log(typeof condition);

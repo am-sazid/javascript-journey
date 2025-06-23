f0a6a4008211fb07a2c6c84f7b8221c4493d9d23
am-sazid <amsazid.cse@gmail.com>
2025-06-23 23:39:53 +0600

    compare to python

--- intro-js/test.py
+++ intro-js/test.py
@@ -1,3 +1,10 @@
 
 print(10 >= 10)
 
+print(10 > 10)  # True
+print(10 < 10)  # False
+print(10 <= 10)  # True
+print(10 == 10)  # True
+print(10 != 10)  # False
+print(10 == 10.0)  # True
+print(10 == "10")  # False

cf682be302f965d7db3eb1f138702355df06fc7b
am-sazid <amsazid.cse@gmail.com>
2025-06-29 22:24:59 +0600

    variable type

---
+++ intro-js/conditional.js
@@ -0,0 +1,9 @@
+var number = 22;
+
+if (number > 20){
+    console.log("YES")
+
+}
+else{
+    console.log("NO")
+}
--- intro-js/type.js
+++ intro-js/type.js
@@ -9,4 +9,4 @@ var nothing = null;
 console.log(typeof nothing); // "object" (this is a known JavaScript quirk)
 
 var condition = 5 > 3;
-console.log(typeof condition);
+console.log(typeof condition); // 

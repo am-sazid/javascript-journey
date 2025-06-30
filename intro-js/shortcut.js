d8171fd5c7d6d8cd575b69c3a1409ed52a735d0a
am-sazid <amsazid.cse@gmail.com>
2025-06-30 23:22:48 +0600

    Ternary oparator

---
+++ intro-js/shortcut.js
@@ -0,0 +1,17 @@
+
+const isPassed = false;
+// if (isPassed === true)
+// if (isPassed){
+//     console.log("Congratulation")
+// }
+// else{
+//     console.log("Fail")
+// }
+
+
+if (!isPassed){
+    console.log("Fail")
+}
+else{
+    console.log("Congratulation")
+}
---
+++ intro-js/ternary.js
@@ -0,0 +1,10 @@
+
+let price = 5100 ;
+const isLeader = false;
+
+price = isLeader === true ?
+            price >= 1000 ?
+            price /2 : 0
+            :price + 1000
+
+console.log(price)

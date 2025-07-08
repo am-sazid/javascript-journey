38bf1ac0854f716447aa2718e243a22e014ed6f0
am-sazid <amsazid.cse@gmail.com>
2025-07-08 22:38:42 +0600

    for loop

--- Loop/for_loop.js
+++ Loop/for_loop.js
@@ -1,8 +1,13 @@
 
-const num = [12, 34, 1, 76, 29, 19];
+// const num = [12, 34, 1, 76, 29, 19];
 
-console.log(num);
+// console.log(num);
 
-for (const number of num ){
-    console.log(number);
-}
+// for (const number of num ){
+//     console.log(number);
+// }
+
+
+var word = "Programming";
+console.log(word.charAt(2));
+console.log(word.charAt(4));

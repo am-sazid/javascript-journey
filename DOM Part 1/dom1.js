879dc0053048ecdd90bbfa9f85d541eb4e252a83
am-sazid <amsazid.cse@gmail.com>
2025-07-24 19:28:16 +0600

    Learn DOM

---
+++ DOM Part 1/dom1.js
@@ -0,0 +1,26 @@
+
+
+
+// console.dir(window.document)
+// console.log(document.body)
+// console.dir(document.body.childNodes[1])
+// document.body.childNodes[3].innerText = "abcd";
+
+// let heading = document.getElementById("heading")
+// console.dir(heading)
+
+// let header = document.getElementsByClassName("header");
+// console.dir(header);
+// console.log(header);
+
+// let para = document.getElementsByTagName("p");
+// console.dir(para);
+
+let element = document.querySelector(".header");
+console.dir(element);
+
+let eleme = document.querySelectorAll(".header");
+console.dir(eleme);
+
+
+

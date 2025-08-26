2b19f6df71902834ae33a5069bbe77e9a8b5b3f3
am-sazid <amsazid.cse@gmail.com>
2025-08-27 02:04:41 +0600

    Es6 Intro

---
+++ ES6 intro/arrow.js
@@ -0,0 +1,16 @@
+// console.log("AM Sazid"
+
+
+function add(a,b) {
+    return a + b;
+}
+
+// Arrow Function 
+
+const add2 = (a,b) => a + b;
+
+
+console.log(add2(20,30));
+console.log(add(10,10));
+
+
---
+++ ES6 intro/spread.js
@@ -0,0 +1,17 @@
+
+
+// const max = Math.max(32,42,12,1,434,1311,2313)
+// console.log(max);
+
+
+
+
+const actor = {
+    name : "Sazid",
+    age : 18,
+    money : 1000029283,
+}
+
+const {name,age,phone} = actor
+
+console.log(phone,name);

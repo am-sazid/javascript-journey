aa02633f74a392f6ded7a8ba7ac7580dddcfe1ca
am-sazid <amsazid.cse@gmail.com>
2025-08-30 23:48:46 +0600

    Explore ES6

---
+++ Explore ES6/map.js
@@ -0,0 +1,9 @@
+const number = [3, 4, 6, 2, 6, 8];
+
+const double = number.map((n) => n * 2);
+console.log(double);
+
+const friends = ["john", "Tom", "Oliver", "Michel"];
+
+const firstLetter = friends.map((name) => name[0]);
+console.log(firstLetter);
---
+++ Explore ES6/recap.js
@@ -0,0 +1,5 @@
+console.log("....Hello ES6....");
+const color = document.getElementById('button').addEventListener("click",function(){
+    document.body.style.backgroundColor = 'red'
+});
+

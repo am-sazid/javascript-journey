344e4ea8f0dcdc25dd84d31fd91df2dcf5109515
am-sazid <amsazid.cse@gmail.com>
2025-09-01 15:48:34 +0600

    update

---
+++ common js concept/type.js
@@ -0,0 +1,31 @@
+
+
+document.getElementById('color').addEventListener('click',function(){
+    document.body.style.backgroundColor = "black";
+    const text = document.getElementById('text');
+    text.style.color = "white"
+    console.log("Button Clicked");
+    
+})
+document.getElementById('color1').addEventListener('click',function(){
+    document.body.style.backgroundColor = "lightblue";
+    const text = document.getElementById('text');
+    text.style.color = "white"
+    console.log("Button Clicked");
+    
+})
+document.getElementById('color2').addEventListener('click',function(){
+    document.body.style.backgroundColor = "orange";
+    const text = document.getElementById('text');
+    text.style.color = "white"
+    console.log("Button Clicked");
+    
+})
+
+document.getElementById('color2').addEventListener('click',function(){
+    document.body.style.backgroundColor = "red";
+    const text = document.getElementById('text');
+    text.style.color = "white"
+    console.log("Button Clicked");
+    
+})

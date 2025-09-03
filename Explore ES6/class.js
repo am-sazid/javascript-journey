8a6529f984f6e565dbb6b01c1bda1145c361eaad
am-sazid <amsazid.cse@gmail.com>
2025-09-03 10:49:44 +0600

    compare 2 value

---
+++ Explore ES6/class.js
@@ -0,0 +1,20 @@
+
+
+// class person{
+//     constructor(name,age){
+//         this.name = name ;
+//         this.age = age ;
+//         console.log(name,age);
+//     }
+// }
+
+// new person("Sazid" ,19)
+
+const person = {
+    name: "Sazid",
+    age : 19,
+    3 : "third"
+}
+
+console.log(person.name)
+console.log(person[3])

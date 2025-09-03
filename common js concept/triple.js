bc092e1f4e9ddd9ef932df217c43d0ad9194e040
am-sazid <amsazid.cse@gmail.com>
2025-09-03 10:49:22 +0600

    comapre 2 value

---
+++ common js concept/triple.js
@@ -0,0 +1,14 @@
+
+const first = 2;
+const second = "2";
+// this line make this value are not equal
+// if(first === second){
+    // this make this value are equal 
+if(first == second){
+    console.log("Value are equal");
+    
+}
+else{
+    console.log("Value are not equal");
+    
+}
